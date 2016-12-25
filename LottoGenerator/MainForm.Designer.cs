cfb353f6408e29f34e38eac6ac594755518a60a4
Ahn, JeongHan <pok00pok00@gmail.com>
2016-12-26 08:22:18 +0900

    test

---
+++ LottoGenerator/MainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace LottoGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.numberList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate(Random)";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Generate(Poisson)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // numberList
+            // 
+            this.numberList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.numberList.Location = new System.Drawing.Point(12, 159);
+            this.numberList.Name = "numberList";
+            this.numberList.Size = new System.Drawing.Size(332, 131);
+            this.numberList.TabIndex = 2;
+            this.numberList.UseCompatibleStateImageBehavior = false;
+            this.numberList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "";
+            this.columnHeader1.Width = 0;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader2.Width = 43;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader3.Width = 43;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader4.Width = 43;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader5.Width = 43;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader6.Width = 43;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "";
+            this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader7.Width = 43;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Bonus";
+            this.columnHeader8.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader8.Width = 50;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 302);
+            this.Controls.Add(this.numberList);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView numberList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+    }
+}
+
---
+++ LottoGenerator/MainForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace LottoGenerator
+{
+    public partial class MainForm : Form
+    {
+        private static Excel.Workbook MyBook = null;
+        private static Excel.Application MyApp = null;
+        private static Excel.Worksheet MySheet = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            numberList.GridLines = true;
+            numberList.FullRowSelect = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            const int NumCount = 45;
+            string sFileName;
+            int[,] fullNumberList;
+            int[] wholeNumbers = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45};
+            int[] numberValues = new int[NumCount];
+            int[] bonusNumberValues = new int[NumCount];
+            int lastRow;
+            int tempNumber;
+            int[,] luckyNumbers = new int[9,7];
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                sFileName = openFileDialog1.FileName;
+            else
+                return;
+            
+            MyApp  = new Excel.Application();
+            MyApp.Visible = false;
+
+            MyBook = MyApp.Workbooks.Open(sFileName);
+            MySheet = MyBook.Sheets[1];
+
+            lastRow = MySheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+            fullNumberList = new int[lastRow, 7];
+            for (int index = 1; index<=lastRow; index++)
+            {
+                Array MyValues = (System.Array)MySheet.get_Range("A" + index.ToString(), "I" + index.ToString()).Cells.Value;
+                for (int arrIndex = 0; arrIndex <=6; arrIndex++)
+                {
+                    fullNumberList[index - 1, arrIndex] = Convert.ToInt16(MyValues.GetValue(1, arrIndex + 3).ToString());
+                    if (arrIndex == 6)
+                        bonusNumberValues[fullNumberList[index - 1, arrIndex] - 1]++;
+                    else
+                        numberValues[fullNumberList[index - 1, arrIndex] - 1]++;                                          
+                }                                
+            }            
+            MyBook.Close();
+            MyApp.Quit();
+
+            // 1. Pick top 6 frequent numbers, 2. Pick top 6 rare numbers
+            Array.Sort(numberValues, wholeNumbers);
+            luckyNumbers[0, 0] = wholeNumbers[44];
+            luckyNumbers[0, 1] = wholeNumbers[43];
+            luckyNumbers[0, 2] = wholeNumbers[42];
+            luckyNumbers[0, 3] = wholeNumbers[41];
+            luckyNumbers[0, 4] = wholeNumbers[40];
+            luckyNumbers[0, 5] = wholeNumbers[39];
+            luckyNumbers[1, 0] = wholeNumbers[0];
+            luckyNumbers[1, 1] = wholeNumbers[1];
+            luckyNumbers[1, 2] = wholeNumbers[2];
+            luckyNumbers[1, 3] = wholeNumbers[3];
+            luckyNumbers[1, 4] = wholeNumbers[4];
+            luckyNumbers[1, 5] = wholeNumbers[5];
+            Array.Sort(wholeNumbers);
+            Array.Sort(bonusNumberValues, wholeNumbers);
+            luckyNumbers[0, 6] = wholeNumbers[44];
+            luckyNumbers[1, 6] = wholeNumbers[0];
+
+            // 3. Poisson distribute - it's same to rare numbers
+
+            // 4. Fibonacci numbers with my birthday
+            luckyNumbers[2, 0] = 8;
+            luckyNumbers[2, 1] = 2;
+            for(int index = 2; index<=6; index++)
+            {
+                tempNumber = luckyNumbers[2, index - 2] + luckyNumbers[2, index - 1];
+                if (tempNumber <= 45)
+                    luckyNumbers[2, index] = tempNumber;
+                else
+                {
+                    tempNumber = tempNumber - 45;
+                    luckyNumbers[2, index] = tempNumber;
+                    if (FindValue(luckyNumbers, 2, tempNumber))
+                        luckyNumbers[2, index] = tempNumber + luckyNumbers[2, index - 1];                    
+                }
+            }
+
+            // 5. Fibonacci numbers with Father's birthday
+          
+            // 6. Fibonacci numbers with Mother's birthday
+
+            // 7. Fibonacci numbers with sister's birthday
+
+            // 8. Fibonacci numbers with Gayoon's birthday
+
+            // 9. Fibonacci numbers with Nayoon's birthday
+
+            // 10. Fibonacci numbers with wife's birthday
+            for (int index = 0; index <=8; index++)
+            {
+                ListViewItem item = new ListViewItem();
+                item.SubItems.Add(luckyNumbers[index, 0].ToString());
+                item.SubItems.Add(luckyNumbers[index, 1].ToString());
+                item.SubItems.Add(luckyNumbers[index, 2].ToString());
+                item.SubItems.Add(luckyNumbers[index, 3].ToString());
+                item.SubItems.Add(luckyNumbers[index, 4].ToString());
+                item.SubItems.Add(luckyNumbers[index, 5].ToString());
+                item.SubItems.Add(luckyNumbers[index, 6].ToString());
+                numberList.Items.Add(item);
+            }
+        }
+
+        private bool FindValue(int[,] numArray, int arrindex, int value)
+        {
+            bool bFind = false;
+            int colIndex;
+            for (colIndex = 0; colIndex <= 5; colIndex++)
+            {
+                if (numArray[arrindex, colIndex] == value)
+                {
+                    bFind = true;
+                    break;
+                }
+            }
+            return bFind;
+        }
+    }
+}
